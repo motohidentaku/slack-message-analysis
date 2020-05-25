2be08901d990b7f3478976dd700a8f20ea74c111
Kazuki Oikawa <k@oikw.org>
2020-05-26 00:31:42 +0900

    チームごとの集計機能を追加

--- slack_message_analysis/leaderboard.py
+++ slack_message_analysis/leaderboard.py
@@ -1,109 +1,37 @@
 from argparse import ArgumentParser, Namespace
-from datetime import datetime, timedelta
+from datetime import datetime
 from typing import Callable, Counter
-import sys
 
 from sqlalchemy import func
 
 from .common import (
-    setup_common_args, setup_token_args, setup_dryrun_args, datetime_parser,
-    create_slack_client)
+    setup_common_args, setup_token_args, setup_date_range_args, post,
+    setup_post_args, get_date_range, get_date_range_str)
 from .models import init_db, transaction, Channel, User, Message
 
 
 def init_argparser(create_parser: Callable[..., ArgumentParser]) -> None:
-    parser = setup_common_args(setup_token_args(setup_dryrun_args(
-        create_parser(
+    parser = setup_common_args(setup_token_args(setup_date_range_args(
+        setup_post_args(create_parser(
             'leaderboard', help='ユーザごとの発言/リアクション数、チャンネルごとの発言数、'
             'リアクションの利用数、チーム単位の発言数を集計し順位表を作成します。\n'
             '--sinceと--until または --day または --week または --month を指定する必要があります。'
-        ))))
-    parser.add_argument(
-        '--post',
-        help='ポスト先チャンネルIDを指定します。--dry-run未指定時は必須オプションです')
-    parser.add_argument(
-        '--since',
-        help='集計開始日時(ISO8601)を指定します',
-        type=datetime_parser)
-    parser.add_argument(
-        '--until',
-        help='集計終了日時(ISO8601)を指定します',
-        type=datetime_parser)
+        )))))
     parser.add_argument(
         '-n',
         default=10,
         help='上位何位まで表示するかを指定します。(デフォルト: 10位)',
         type=int)
-    parser.add_argument(
-        '--day', action='store_true',
-        help='昨日の投稿を集計対象とします')
-    parser.add_argument(
-        '--week', action='store_true',
-        help='先週の投稿を集計対象とします')
-    parser.add_argument(
-        '--month', action='store_true',
-        help='先週の投稿を集計対象とします')
     parser.set_defaults(func=run)
 
 
 def run(args: Namespace) -> None:
-    # DB初期化
     init_db(args.db)
-
-    # 集計日時の範囲を取得
-    since, until = None, None
-    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    if args.day:
-        since = today - timedelta(days=1)
-        until = today
-    elif args.week:
-        until = today - timedelta(days=today.weekday())
-        since = until - timedelta(days=7)
-    elif args.month:
-        until = datetime(year=today.year, month=today.month, day=1)
-        tmp = until - timedelta(days=1)
-        since = datetime(year=tmp.year, month=tmp.month, day=1)
-    else:
-        if not (args.since and args.until):
-            print('集計日時の範囲指定が必要です', file=sys.stderr)
-            return
-        since, until = args.since, args.until
-
-    assert since
-    assert until
+    since, until = get_date_range(args)
 
     _user_posts(since, until, args)
     _channel_posts(since, until, args)
     _reactions(since, until, args)
-
-
-def _post(msg: str, args: Namespace) -> None:
-    client = create_slack_client(args)
-    if args.post is None:
-        print('集計結果ポスト先のチャンネルIDを指定してください',
-              file=sys.stderr)
-        sys.exit(1)
-    client.chat_postMessage(channel=args.post, blocks=[{
-        'type': 'section',
-        'text': {
-            'type': 'mrkdwn',
-            'text': msg
-        }
-    }, {
-        'type': 'divider'
-    }])
-
-
-def _get_range_msg(since: datetime, until: datetime, args: Namespace) -> str:
-    if args.day:
-        return '昨日'
-    if args.week:
-        return '先週'
-    if (until - since).days == 1:
-        return since.date().isoformat()
-    return '{}〜{}'.format(
-        since.date().isoformat(),
-        (until - timedelta(days=1)).date().isoformat())
 
 
 def _user_posts(since: datetime, until: datetime, args: Namespace) -> None:
@@ -123,14 +51,14 @@ def _user_posts(since: datetime, until: datetime, args: Namespace) -> None:
         q = s.query(sq.c.count, User.name).join(User, sq.c.user_id == User.id)
 
         output = [
-            '{} の発言数ランキング'.format(_get_range_msg(since, until, args))]
+            '{} の発言数ランキング'.format(get_date_range_str(since, until, args))]
         for i, (count, name) in enumerate(q):
             output.append('{}. {} ({} posts)'.format(i + 1, name, count))
 
     print('\n'.join(output))
     print()
     if not args.dry_run and len(output) > 1:
-        _post('\n'.join(output), args)
+        post(args, '\n'.join(output))
 
 
 def _channel_posts(since: datetime, until: datetime, args: Namespace) -> None:
@@ -152,14 +80,14 @@ def _channel_posts(since: datetime, until: datetime, args: Namespace) -> None:
         ).join(Channel, sq.c.channel_id == Channel.id)
 
         output = [
-            '{} の人気チャンネルランキング'.format(_get_range_msg(since, until, args))]
+            '{} の人気チャンネルランキング'.format(get_date_range_str(since, until, args))]
         for i, (count, name) in enumerate(q):
             output.append('{}. #{} ({} posts)'.format(i + 1, name, count))
 
     print('\n'.join(output))
     print()
     if not args.dry_run and len(output) > 1:
-        _post('\n'.join(output), args)
+        post(args, '\n'.join(output))
 
 
 def _reactions(since: datetime, until: datetime, args: Namespace) -> None:
@@ -188,7 +116,7 @@ def _reactions(since: datetime, until: datetime, args: Namespace) -> None:
 
     output_user = [
         '{} のリアクション数ランキング'.format(
-            _get_range_msg(since, until, args))]
+            get_date_range_str(since, until, args))]
     for i, (name, count) in enumerate(user_leaderboard):
         output_user.append('{}. {} ({} reactions)'.format(i + 1, name, count))
     print('\n'.join(output_user))
@@ -196,7 +124,7 @@ def _reactions(since: datetime, until: datetime, args: Namespace) -> None:
 
     output_reaction = [
         '{} の人気リアクションランキング'.format(
-            _get_range_msg(since, until, args))]
+            get_date_range_str(since, until, args))]
     for i, (name, count) in enumerate(reaction_counts.most_common(args.n)):
         output_reaction.append('{}. :{}: ({} 回)'.format(i + 1, name, count))
     print('\n'.join(output_reaction))
@@ -204,6 +132,6 @@ def _reactions(since: datetime, until: datetime, args: Namespace) -> None:
 
     if not args.dry_run:
         if len(output_user) > 1:
-            _post('\n'.join(output_user), args)
+            post(args, '\n'.join(output_user))
         if len(output_reaction) > 1:
-            _post('\n'.join(output_reaction), args)
+            post(args, '\n'.join(output_reaction))
---
+++ slack_message_analysis/team.py
@@ -0,0 +1,73 @@
+from argparse import ArgumentParser, Namespace
+import csv
+from typing import Callable, Counter
+
+from sqlalchemy import func
+
+from .common import (
+    setup_common_args, setup_token_args, setup_date_range_args, post,
+    setup_post_args, get_date_range, get_date_range_str)
+from .models import init_db, transaction, User, Message
+
+
+def init_argparser(create_parser: Callable[..., ArgumentParser]) -> None:
+    parser = setup_common_args(setup_token_args(setup_date_range_args(
+        setup_post_args(create_parser(
+            'team', help='チームごとの発言数を集計します。\n'
+            '--sinceと--until または --day または --week または --month を指定する必要があります。'
+        )))))
+    parser.add_argument(
+        '-n',
+        default=10,
+        help='上位何位まで表示するかを指定します。(デフォルト: 10位)',
+        type=int)
+    parser.add_argument(
+        '--team',
+        default='team_master.csv',
+        help='メンバとチームを定義づけたCSVファイル')
+    parser.set_defaults(func=run)
+
+
+def run(args: Namespace) -> None:
+    init_db(args.db)
+    since, until = get_date_range(args)
+
+    # CSVを読み込みユーザ(e-mail)とチーム名のマッピングを取得する
+    team_master = {}
+    with open(args.team, newline='', encoding='utf8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(reader)  # skip header
+        for email, team_name, _, _ in reader:
+            team_master[email] = team_name
+
+    team_posts = Counter[str]()
+    with transaction() as s:
+        sq = s.query(
+            Message.user_id.label('user_id'),
+            func.count(Message.user_id).label('count'),
+        ).filter(
+            Message.timestamp >= since.timestamp(),
+            Message.timestamp < until.timestamp(),
+            Message.subtype == '',
+        ).group_by(
+            Message.user_id,
+        ).order_by(
+            func.count(Message.user_id).desc(),
+        ).limit(args.n).subquery()
+        q = s.query(sq.c.count, User.email).join(User, sq.c.user_id == User.id)
+
+        for count, email in q:
+            tmp = team_master.get(email)
+            if tmp is None:
+                continue
+            team_posts[tmp] += count
+
+    output = ['{} のチーム発言数ランキング'.format(
+        get_date_range_str(since, until, args))]
+    for i, (name, count) in enumerate(team_posts.most_common(args.n)):
+        output.append('{}. {} ({} posts)'.format(i + 1, name, count))
+    print('\n'.join(output))
+    print()
+
+    if not args.dry_run and len(output) > 1:
+        post(args, '\n'.join(output))
